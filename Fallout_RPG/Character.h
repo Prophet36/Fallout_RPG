59c3f8bf64ff3909b681027134577cbb81267783
Mateusz Warchoł <mat.warchol.93@gmail.com>
2018-01-20 23:54:45 +0100

    Add character base class

---
+++ Fallout_RPG/Character.h
@@ -0,0 +1,28 @@
+// Base character class
+
+#pragma once
+
+#include <string>
+
+class Character
+{
+public:
+	Character();
+	~Character();
+
+private:
+	const std::string m_name;
+	int m_level;
+	int m_strength;
+	int m_agility;
+	int m_vitality;
+	int m_perception;
+	int m_intelligence;
+	int m_luck;
+	int m_health;
+	int m_reduction;
+	int m_evasion;
+	int m_speed;
+	int m_critical;
+};
+
